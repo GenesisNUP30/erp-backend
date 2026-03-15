ba6b35f74e1a8841454c05ffc2ea79596279ec97
GenesisNUP30 <jdirproy@gmail.com>
2026-03-16 00:15:39 +0100

    Migracion de la tabla Tareas creada y ejecutada

---
+++ database/migrations/2026_03_15_231420_create_tareas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tareas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('parcela_id')->constrained('parcelas')->onDelete('restrict');
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('set null');
+
+            $table->string('titulo');
+            $table->string('tipo');
+            $table->date('fecha');
+            $table->decimal('horas_estimadas', 8, 2);
+            $table->text('descripcion')->nullable();
+            $table->enum('estado', ['pendiente', 'en_progreso', 'finalizada', 'cancelada'])
+                ->default('pendiente');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tareas');
+    }
+};
