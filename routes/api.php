1bc3bc56ba952249b4526987f1a03e6134872b64
GenesisNUP30 <jdirproy@gmail.com>
2026-03-16 02:46:28 +0100

    Endpoint de parcelas añadido

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ParcelaController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -13,3 +14,4 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::apiResource('/trabajadores', UserController::class);
 
+Route::apiResource('/parcelas', ParcelaController::class);
